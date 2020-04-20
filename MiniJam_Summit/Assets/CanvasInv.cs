090e923ef619a29233368394623a707d85d4156a
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2020-04-20 03:58:52 +0200

    main menu script (not working)

---
+++ MiniJam_Summit/Assets/CanvasInv.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasInv : MonoBehaviour
+{
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    CameraLerpEvent camera_bool;
+    public GameObject camera_manager;
+
+    void Start()
+    {
+        camera_bool = camera_manager.GetComponent<CameraLerpEvent>();
+        isShowing = true;
+    }
+
+    void Update()
+    {
+        if (camera_bool.startMaleLerp)
+        {
+            isShowing = true;
+            menu.SetActive(isShowing);
+        }
+        else if(camera_bool.startMaleLerp == false)
+        {
+            isShowing = false;
+            menu.SetActive(isShowing);
+        }
+    }
+}
